dd0b81603a42f1693d93ea50c79aa45d724f3384
Awourrobberrt <sophierobberrt@gmail.com>
2021-03-16 15:01:49 +0300

    javascript assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function fruits(){
+    var fruits=["mango","orange","banana","apple","avocado","passion","kiwi"]
+    fruits.forEach(element=>console.log(element))
+    
+ }
+ fruits();
+    
+
+    
+    
+    
+    
